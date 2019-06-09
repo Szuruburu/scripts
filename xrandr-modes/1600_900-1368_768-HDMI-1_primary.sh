4ccdfe376d3f782e7910aaef4032e50ab2db3d21
Szuruburu <szuru.buru@hotmail.com>
2019-06-09 23:42:42 +0200

    is it fixed?; yes, it is

--- xrandr-modes/1600_900-1368_768-HDMI-1_primary.sh
+++ xrandr-modes/1600_900-1368_768-HDMI-1_primary.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
 xrandr --addmode HDMI-1 1600x900_60.00
-xrandr --output HDMI-1 --primary --mode 1368x768 --pos 1600x132 --rotate normal --output HDMI-1 --mode 1600x900_60.00 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --mode 1368x768 --pos 1600x132 --rotate normal --output HDMI-1 --primary --mode 1600x900_60.00 --pos 0x0 --rotate normal
